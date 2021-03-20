8e8009b65f7b76686f5cc312a1a5ae0c6dd0e6d8
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-03-20 18:22:39 -0500

    Logout Successful

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -3,32 +3,41 @@ const { Blog } = require('../models')
 const withAuth = require('../utils/auth')
 
 router.get('/', async (req, res) => {
-    try {
-        const blogData = await Blog.findAll({})
-        const blogs = blogData.map((blog) => blog.get({ plain: true }));
-        res.render('homepage', {
-            blogs,
-            loggedIn: req.session.logged_in
-        });
-    } catch (err) {
-        res.status(500).json(err)
-    }
-}) 
+  try {
+    const blogData = await Blog.findAll({})
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+    res.render('homepage', {
+      loggedIn: req.session.logged_in,
+      blogs,
+
+    });
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
 router.get('/dash', withAuth, (req, res) => {
-    res.render('dash');
+  res.render('dash', {
+    loggedIn: req.session.logged_in
   });
+});
 
 router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-      res.redirect('/');
-      return;
-    }
-    res.render('login');
-  });
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
 
 router.get('/signup', (req, res) => {
-    res.render('signup');
+  res.render('signup');
+});
+
+router.get('/logout', (req, res) => {
+  res.render('logout', {
+    loggedIn: req.session.logged_in
   });
+});
 
 module.exports = router;
