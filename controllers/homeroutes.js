7efb6ddd0f3a34b1984c90435142fff7a39024eb
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-03-20 18:03:09 -0500

    Login Working W/O password validation

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -8,6 +8,7 @@ router.get('/', async (req, res) => {
         const blogs = blogData.map((blog) => blog.get({ plain: true }));
         res.render('homepage', {
             blogs,
+            loggedIn: req.session.logged_in
         });
     } catch (err) {
         res.status(500).json(err)
