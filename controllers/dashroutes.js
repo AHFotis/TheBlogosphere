789bb7bc3fea8b91258fb11f75e08ff4a7a8d0f0
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-03-22 12:32:10 -0500

    Single Blog Returning with Update and Delete Buttons

---
+++ controllers/dashroutes.js
@@ -0,0 +1,91 @@
+const router = require('express').Router();
+const { Blog, User, Comments } = require('../models')
+const withAuth = require('../utils/auth')
+
+//render dashboard
+router.get('/', withAuth, async (req, res) => {
+    try {
+      const blogData = await Blog.findAll({
+        where: {
+          user_id: req.session.user_id,
+        },
+        attributes: ["id", "content", "title", "createdAt"],
+      })
+  
+      console.log("TEST" + blogData)
+      if (!blogData) {
+        res.status(404).json({
+          message: "No post found with this id"
+        });
+        return;
+      }
+  
+      const blogs = blogData.map((blog) => blog.get({ plain: true }));
+      console.log(blogs)
+  
+      res.render('dash', {
+        blogs,
+        loggedIn: req.session.logged_in
+      })
+  
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  })
+  
+  router.get('/:id', withAuth, async (req, res) => {
+    try {
+      const blogData = await Blog.findOne({
+        where: {
+          id: req.params.id,
+        },
+        attributes: ["id", "content", "title", "createdAt"],
+      })
+  
+      if (!blogData) {
+        res.status(404).json({
+          message: "No post found with this id"
+        });
+        return;
+      }
+  
+      const blog = blogData.get({ plain: true });
+      console.log(blog)
+  
+      res.render('singleBlog', {
+        blog,
+        loggedIn: req.session.logged_in
+      })
+  
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  })
+
+  
+  //post new blog
+  router.post("/newblog", async (req, res) => {
+    const user_id = req.session.user_id
+    try {
+        const newBlog = await Blog.create({
+            title: req.body.title,
+            content: req.body.content,
+            user_id: user_id
+        });
+        res.status(200).json(newBlog);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+  })
+  
+  //render new blog page
+  router.get("/newblog", withAuth, async (req, res) => {
+    if (!req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+    res.render('newblog');
+  })
+  
+  
+  module.exports = router;
--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -43,60 +43,60 @@ router.get('/logout', (req, res) => {
   });
 });
 
-//render dashboard
-router.get('/dash', withAuth, async (req, res) => {
-  try {
-    const blogData = await Blog.findAll({
-      where: {
-        user_id: req.session.user_id,
-      },
-      attributes: ["id", "content", "title", "createdAt"],
-    })
+// //render dashboard
+// router.get('/dash', withAuth, async (req, res) => {
+//   try {
+//     const blogData = await Blog.findAll({
+//       where: {
+//         user_id: req.session.user_id,
+//       },
+//       attributes: ["id", "content", "title", "createdAt"],
+//     })
 
-    console.log("TEST" + blogData)
-    if (!blogData) {
-      res.status(404).json({
-        message: "No post found with this id"
-      });
-      return;
-    }
+//     console.log("TEST" + blogData)
+//     if (!blogData) {
+//       res.status(404).json({
+//         message: "No post found with this id"
+//       });
+//       return;
+//     }
 
-    const blogs = blogData.map((blog) => blog.get({ plain: true }));
-    console.log(blogs)
+//     const blogs = blogData.map((blog) => blog.get({ plain: true }));
+//     console.log(blogs)
 
-    res.render('dash', {
-      blogs,
-      loggedIn: req.session.logged_in
-    })
+//     res.render('dash', {
+//       blogs,
+//       loggedIn: req.session.logged_in
+//     })
 
-  } catch (err) {
-    res.status(500).json(err)
-  }
-})
+//   } catch (err) {
+//     res.status(500).json(err)
+//   }
+// })
 
-//post new blog
-router.post("/dash/newblog", async (req, res) => {
-  const user_id = req.session.user_id
-  try {
-      const newBlog = await Blog.create({
-          title: req.body.title,
-          content: req.body.content,
-          user_id: user_id
-      });
-      res.status(200).json(newBlog);
-  } catch (err) {
-      res.status(400).json(err);
-  }
-})
+// //post new blog
+// router.post("/dash/newblog", async (req, res) => {
+//   const user_id = req.session.user_id
+//   try {
+//       const newBlog = await Blog.create({
+//           title: req.body.title,
+//           content: req.body.content,
+//           user_id: user_id
+//       });
+//       res.status(200).json(newBlog);
+//   } catch (err) {
+//       res.status(400).json(err);
+//   }
+// })
 
-//render new blog page
-router.get("/dash/newblog", withAuth, async (req, res) => {
-  if (!req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
-  res.render('newblog');
-})
+// //render new blog page
+// router.get("/dash/newblog", withAuth, async (req, res) => {
+//   if (!req.session.logged_in) {
+//     res.redirect('/');
+//     return;
+//   }
+//   res.render('newblog');
+// })
 
 
 module.exports = router;
